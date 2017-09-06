fde6157286e1d327563fd5a7da09f2ef15b1d441
aerograf333 <rkhorenko@curiousit.eu>
2017-09-06 10:49:49 +0300

    first commit

---
+++ Pods/Target Support Files/Pods-addMobDemo/Pods-addMobDemo-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_addMobDemo : NSObject
+@end
+@implementation PodsDummy_Pods_addMobDemo
+@end
--- addMobDemo/ViewController.swift
+++ addMobDemo/ViewController.swift
@@ -7,12 +7,41 @@
 //
 
 import UIKit
+import Firebase
+import GoogleMobileAds
 
-class ViewController: UIViewController {
 
+class ViewController: UIViewController, UIAlertViewDelegate, GADBannerViewDelegate {
+
+ 
+    @IBOutlet weak var bannerView: GADBannerView!
+  
+    var interstitial: GADInterstitial!
+    
+    @IBAction func displayAdd(_ sender: Any) {
+        if interstitial.isReady{
+            interstitial.present(fromRootViewController: self)
+        } else {
+            print("Add wosn't ready")
+        
+        }
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        print("GoggleMobileAds SDK version: " + GADRequest.sdkVersion())
+        bannerView.adUnitID = "ca-app-pub-3940256099942544/2934735716"
+        bannerView.rootViewController = self
+        bannerView.delegate = self
+        bannerView.load(GADRequest())
+        
+         let  requst = GADRequest()
+       
+         interstitial = GADInterstitial(adUnitID: "ca-app-pub-3940256099942544/4411468910")
+        requst.testDevices = [kGADSimulatorID, "12FF8F4C-0CB1-4CCB-B380-605D052C970A" ]
+        interstitial.load(requst)
     }
 
     override func didReceiveMemoryWarning() {
